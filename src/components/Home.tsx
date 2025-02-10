6e56afa8a180b1c89a9a1b61514cef08b82f9cd5
ArnoldMsl <arnold.masselin@gmail.com>
2025-02-10 10:46:32 +0100

    fixed few bugs

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -12,16 +12,16 @@ const Home = () => {
                     <h1 className={'customH1'}>ARNOLD MASSELIN</h1>
                     <img className={'lg:hidden md:block rounded-full w-3/6 drop-shadow-lg scroll-reveal pop'} src='src/assets/images/others/Photo.webp'></img>
                     <h2 className={'customH2'}>Et en tant que
-                        <span className={'font-extrabold xl:text-5xl lg:text-3xl'}> Développeur</span>,<br /> je crée des trucs sur internet</h2>
+                        <span className={'font-extrabold xl:text-4xl lg:text-3xl'}> Développeur</span>,<br /> je crée des trucs sur internet</h2>
                     <p className={'customH2'}>On construit ensemble ?</p>
                 </div>
                 <img className={'lg:block hidden rounded-full lg:w-2/6 drop-shadow-lg scroll-reveal pop'} src='src/assets/images/others/Photo.webp'></img>
             </div>
 
             <div className={'absolute bottom-[10px] md:bottom-[90px] lg:bottom-[90px] xl:bottom-[340px] w-screen'}>
-                <img className={'absolute z-[-4] scroll-reveal animatedY animBottom drop-shadow-custom'} src="src\assets\images\background\herobackground3.png"></img>
-                <img className={'absolute z-[-3] scroll-reveal animatedY speedMedium animBottom'} src="src\assets\images\background\herobackground2.png"></img>
-                <img className={'absolute z-[-1] scroll-reveal animatedY speedSlow animBottom'} src="src\assets\images\background\herobackground1.png"></img>
+                <img className={'absolute z-[-4] scroll-reveal animatedY animBottom drop-shadow-custom'} src="src\assets\images\background\herobackground3.webp"></img>
+                <img className={'absolute z-[-3] scroll-reveal animatedY speedMedium animBottom'} src="src\assets\images\background\herobackground2.webp"></img>
+                <img className={'absolute z-[-1] scroll-reveal animatedY speedSlow animBottom'} src="src\assets\images\background\herobackground1.webp"></img>
             </div>
         </section>
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,12 +8,8 @@ module.exports = {
         bgcolordim: 'var(--bg-color-dim)',
         bgimagecolor: 'var(--bg-image-color)',
       },
-      spacing: {
-        paddingX: '2.5rem',
-        paddingY: '2.5rem',
-      },
       dropShadow: {
-        custom: '0 30px 20px rgba(89, 60, 73, 0.6)',  
+        custom: '0 40px 40px rgba(89, 60, 73, 1)',
       }
     },
   },
